6e93c775b1c63cd2a0c74eb6d0e75462a13b2cb4
Kaival <87368466+Kaival1212@users.noreply.github.com>
2024-01-09 22:43:22 +0000

    added coffee and links

---
+++ app/(Comps)/Coffee.js
@@ -0,0 +1,67 @@
+
+import "./Coffee.css";
+export  default  function  Coffee()  {
+    return (
+        <div className="machine">
+            <div className="upper"></div>
+            <div className="middle">
+                <div className="timer">
+                    <div className="dot1"></div>
+                    <div className="dot2"></div>
+                    <div className="dot3"></div>
+                    <div className="dot4"></div>
+                    <div className="dot5"></div>
+                    <div className="dot6"></div>
+                    <div className="dot7"></div>
+                    <div className="dot8"></div>
+                    <div className="hand"></div>
+                </div>
+                <div className="btn1"></div>
+                <div className="btn2">
+                    <div className="btn0"></div>
+                </div>
+            </div>
+            <div className="b1">
+                <div className="b2"></div>
+                <div className="b3"></div>
+                <div className="b4"></div>
+                <div className="handle">
+                    <div className="h1"></div>
+                    <div className="h2"></div>
+                </div>
+                <div className="coff"></div>
+                <div className="steam">
+                    <div className="s1"></div>
+                </div>
+                <div className="drop"></div>
+            </div>
+
+
+            <div className="back">
+                {/*<div className="brdr">*/}
+                {/*    <div className="d1"></div>*/}
+                {/*    <div className="d2"></div>*/}
+                {/*    <div className="d3"></div>*/}
+                {/*    <div className="d4"></div>*/}
+                {/*    <div className="d5"></div>*/}
+                {/*    <div className="d6"></div>*/}
+                {/*    <div className="d7"></div>*/}
+                {/*    <div className="d8"></div>*/}
+                {/*    <div className="d9"></div>*/}
+                {/*    <div className="d10"></div>*/}
+                {/*    <div className="d11"></div>*/}
+                {/*</div>*/}
+                <div className="cup">
+                    <div className="shadow"></div>
+                    <div className="u"></div>
+                </div>
+            </div>
+            <div className="lower">
+                <div className="btnn"></div>
+                <div className="light"></div>
+                <div className="bottom"></div>
+            </div>
+        </div>
+
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,36 +1,51 @@
 "use client"
 
 import {TypeAnimation} from "react-type-animation";
+import Image from "next/image";
+import { FaGithub ,FaLinkedin } from "react-icons/fa";
+import Coffee from "./(Comps)/Coffee";
+import Link from "next/link";
 
 export default function Home() {
 
   return (
       <section className={"flex flex-col h-full bg-[#03001C] text-[#FFE039] text-4xl font-bold font-playfair"}>
-        <nav className={"flex justify-center items-center"}>
+          <nav className="flex justify-center text-6xl font-bold items-center h-1/6 bg-gradient-to-b from-[#03001C] to-[#252035]  backdrop-filter backdrop-blur-md shadow-md">
           Kaival Patel
         </nav>
-        <section className={"flex-grow flex items-center text-xl justify-start"}>
+        <section className={"flex-grow flex items-center text-sm justify-start"}>
+            <div className={"w-1/2 ml-2"}>
             <TypeAnimation
                 sequence={[
-                    "I'm a Computer Science student with a passion for learning.",
+                    "I'm a Computer Science student with a passion for learning .",
                     1000,
-                    'I have experience with AI using TensorFlow.',
+                    'I have experience with AI using TensorFlow .',
                     500,
-                    'I am exploring the exciting world of Full-stack development.',
+                    'I am exploring the exciting world of Full-stack development .',
                     500,
-                    'I am eager to dive into iOS app development.',
+                    'I am eager to dive into iOS app development .',
                     500,
-                    'I am also interested in game development.',
+                    'I am also interested in game development .',
                     500,
                 ]}
-
                 speed={50}
                 style={{ fontSize: '2em' }}
                 repeat={Infinity}
             />
+            </div>
+
+            <div className={"flex w-1/2  justify-center items-center"}>
+            <Coffee></Coffee>
+            </div>
+
         </section>
-        <footer className={"flex justify-center items-center"}>
-          contact me!!
+        <footer className={"flex mb-3 ml-3 gap-3"}>
+           <Link href={"https://github.com/Kaival1212"} replace={false} target="_blank">
+            <FaGithub height={"1000"} width={"1000"}/>
+           </Link>
+            <Link href={"https://www.linkedin.com/in/kaival-patel-6a3614252/"}replace={false} target="_blank">
+            <FaLinkedin />
+            </Link>
         </footer>
       </section>
   )
